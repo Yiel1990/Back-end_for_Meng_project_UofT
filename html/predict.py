b2dfbfbb2c0d6a201891f9f82a68350687bfcea8
Yiou <zhaoyiou1990@163.com>
2017-01-17 19:21:18 -0500

    Update predict.py
    Accelerating the speed for the sever part

--- html/predict.py
+++ html/predict.py
@@ -30,17 +30,17 @@ classes = ['Angry', 'Disgust', 'Fear', 'Happy','Sad','Surprise','Neutral'];
 weights_path_ICML = './model/Model.1156-0.6729.hdf5'
 model_ICML = model.model_ICML()
 model_ICML.load_weights(weights_path_ICML)
-print("Model loaded successfully")
+#print("Model loaded successfully")
 
 weights_path_VGG = './model/Model.356-0.6796.hdf5'
 model_VGG = model.model_VGG_face()
 model_VGG.load_weights(weights_path_VGG)
-print("Model loaded successfully")
+#print("Model loaded successfully")
 
 VGG_19_weigth_path = "./model/VGG_19_Model.310-0.6668.hdf5"
 model_VGG_19 = model.model_VGG_19_face()
 model_VGG_19.load_weights(VGG_19_weigth_path)
-print("Model loaded successfully")
+#print("Model loaded successfully")
 
 filename = sys.argv[1]
 img = array((Image.open(filename).convert('L')).resize((img_rows, img_cols)),'f')
@@ -48,9 +48,9 @@ tmpdata = processing.Process(img)
 inputdata = np.empty((1,channel,img_rows,img_cols),dtype="float32")
 inputdata[0,0,:,:] = tmpdata
 
-out_ICML = model_ICML.predict_proba(inputdata,batch_size=128, verbose=1)
-out_VGG = model_VGG.predict_proba(inputdata,batch_size=128, verbose=1)
-out_VGG_19 =model_VGG_19.predict_proba(inputdata,batch_size=128, verbose=1)
+out_ICML = model_ICML.predict_proba(inputdata,batch_size=128, verbose=0)
+out_VGG = model_VGG.predict_proba(inputdata,batch_size=128, verbose=0)
+out_VGG_19 =model_VGG_19.predict_proba(inputdata,batch_size=128, verbose=0)
 
 final_out = out_ICML + out_VGG + out_VGG_19
 
